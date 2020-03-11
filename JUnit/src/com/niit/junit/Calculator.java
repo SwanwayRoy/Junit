9c1b47044ac14f5394ddda2f1507d53410354b84
Swanway Roy <royswanway@gmail.com>
2020-03-11 16:04:40 +0530

    Junit Code Modification - 2

--- JUnit/src/com/niit/junit/Calculator.java
+++ JUnit/src/com/niit/junit/Calculator.java
@@ -24,4 +24,18 @@ public class Calculator
 			throw new ArithmeticException();
 		return a / b;
 	}
+	
+	public static void main (String args [])
+	{
+		Calculator calObj = new Calculator();
+		int a, b;
+		a = 20;
+		b = 10;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("Sum : " + calObj.add(a, b));
+		System.out.println("Difference : " + calObj.substract(a, b));
+		System.out.println("Product : " + calObj.multiply(a, b));
+		System.out.println("Quotient : " + calObj.divide(a, b));
+	}
 }
